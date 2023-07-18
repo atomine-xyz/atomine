b51de061a9b4794b5ead8b16e24395cecab381d4
L4DN0 <ves.artem2005@gmail.com>
2023-07-18 14:16:23 +0300

    Поставил favicon

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -7,6 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Главная | Atom</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link type="Image/x-icon" href="{{ Vite::asset('resources/images/favicon.ico') }}" rel="icon">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@700&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;400;500;600;700;800&display=swap" rel="stylesheet">
 </head>
