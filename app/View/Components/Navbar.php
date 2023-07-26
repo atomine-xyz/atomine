06dd969603c60de81e59c3fbe6008ea002e48b51
L4DN0 <ves.artem2005@gmail.com>
2023-07-26 21:59:48 +0300

    Поменял ссылку на кнопку 'войти' (:3)

--- app/View/Components/Navbar.php
+++ app/View/Components/Navbar.php
@@ -5,17 +5,20 @@ namespace App\View\Components;
 use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Arr;
 
 class Navbar extends Component
 {
     public array $links;
+    public string $url;
 
     /**
      * Create a new component instance.
      */
     public function __construct()
     {
-
+        $this->url = Storage::url(Arr::random(Storage::allFiles('public\animan')));
         $this->links = ['Вики' => "#:wiki", 'Карта' => "https://maps.atomine.xyz", '_Ресурспак' => "https://vk.cc/cmhWS5", '_Discord' => "#:discord"];
     }
 
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\WikiController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Arr;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +19,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('index');
+});
+
+Route::get('/test', function () {
+
 });
 
 Route::get('/discord', function () {
