d70fc516ba17e89bc4ea39994697b3baf74b2c0a
L4DN0 <ves.artem2005@gmail.com>
2023-07-26 20:21:07 +0300

    BUGFIX: добавил wiki.css в список файлов бандлера Vite

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/css/app.css',
+                'resources/css/wiki.css',
                 'resources/js/app.js',
             ],
             refresh: [
